ed108c69a0e139b268fb4004ad03c653aecccaf0
Carlos Tolentino <carlostolentino@Carloss-iMac.local>
2018-10-28 23:18:14 -0400

    Change

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
-const app = express();
+const http =  require('http');
 const path = require('path');
+
+const app = express();
+
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname + '/dist')));
 // Start the app by listening on the default
 // Heroku port
-app.get('/*', function(req, res) {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
-  });
+});
 
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || '3001';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('Running'));
+
