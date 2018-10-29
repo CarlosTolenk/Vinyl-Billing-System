c2c7f8d4781288b6b5879a7c1bccdd0f39034e22
Carlos Tolentino <carlostolentino@Carloss-iMac.local>
2018-10-28 20:50:09 -0400

    Subir

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
 
-//PathLocationStrategy
-app.get('/*', (req, res) => {
+app.get('/*', function(req, res){
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
 
-console.log('Console Listinging!!!...');
+console.log('Console Listinging!');
